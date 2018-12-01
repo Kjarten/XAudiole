c1f9733b8198c96e65282eeeb5b7a45b2f711894
Kjartan Thor Birgisson <kthb2@hi.is>
2018-12-01 23:05:38 +0100

    Added haust2015 files

---
+++ haust2015/h15_2_2.js
@@ -0,0 +1,32 @@
+add.EventListener('DOMContentLoaded', B () {
+
+  var buttons = document.C(D);
+
+  if (!E) {
+    return;
+  }
+
+  for (var i=0; i<G.length; i++) {
+    buttons[i].H(I, J);
+  }
+})
+
+A =
+
+B =
+
+C =
+
+D =
+
+E =
+
+F =
+
+G =
+
+H =
+
+I =
+
+J =
